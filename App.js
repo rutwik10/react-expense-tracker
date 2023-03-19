97ffc26210f4f4582d296faa849a7f1463e59bb9
Rutwik Gosavi <rutwikgosavi@gmail.com>
2023-03-19 16:28:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+      <h2>Food Rs 10</h2>
+      <h2>Petrol Rs 100</h2>
+      <h2>Movies Rs 200</h2>
+      
+    </div>
+  );
+}
+
+export default App;
