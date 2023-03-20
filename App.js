fbc9f84a4a83d73d0d8a126f3e86baa28ee8427e
Rutwik Gosavi <rutwikgosavi@gmail.com>
2023-03-20 16:27:01 +0530

    Update App.js

--- App.js
+++ App.js
@@ -30,30 +30,15 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        location={expenses[0].location}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        location={expenses[1].location}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        location={expenses[2].location}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        location={expenses[3].location}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          
+          title={expense.title}
+          amount={expense.amount}
+          location={expense.location}
+          date={expense.date}
+        />
+      ))}
     </div>
   );
 }
