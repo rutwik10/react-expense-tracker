86dbe689251b9293c40e42b31f04854923de4352
Rutwik Gosavi <rutwikgosavi@gmail.com>
2023-03-20 17:28:37 +0530

    Update ExpenseItem.js

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,14 +1,14 @@
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+    
+
     return (
     <div className='expense-item'>
-        <div>{props.date.toISOString()}</div>
-        <div className='expense-item__description'>
-            <h2>{props.title}</h2>
-            <h2>{props.location}</h2>
-            <div className='expense-item__price'>${props.amount}</div>
-        </div>
+        <ExpenseDate date={props.date}></ExpenseDate>
+        <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
 
     </div>
     )
