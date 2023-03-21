c78efda96929888c176ce24e66f64b98daa7537d
Rutwik Gosavi <rutwikgosavi@gmail.com>
2023-03-21 14:26:18 +0530

    Update ExpenseItem.js

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -2,14 +2,18 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css'
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+    
+    const clickHandler = () => {
+    console.log("Clicked")
+  }
     
 
     return (
     <div className='expense-item'>
         <ExpenseDate date={props.date}></ExpenseDate>
         <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-
+        <button onClick={clickHandler}>Delete Expense</button>
     </div>
     )
 }
